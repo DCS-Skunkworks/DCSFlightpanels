e82d04faab44c1462d3273c60dcfe8a7e53f46e4
c0nnex <connex@coppercoins.de>
2016-03-21 16:30:56 +0100

    Generic pre-build

---
+++ BuildScripts/pre-build-Release.cmd
@@ -0,0 +1,2 @@
+.\UpdateVersion.exe -b Increment -r ddHHmm -i "%1Properties\AssemblyInfo.cs" -o "%1Properties\AssemblyInfo.cs"
+.\UpdateVersion.exe -v File -b Increment -r ddHHmm -i "%1Properties\AssemblyInfo.cs" -o "%1Properties\AssemblyInfo.cs"
