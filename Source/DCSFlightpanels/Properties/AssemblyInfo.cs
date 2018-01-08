79ec7af290bc6fe30a1e802c695b22bddfa56f82
Jerker Dahlblom <jdahlblom@users.noreply.github.com>
2018-01-08 15:45:01 +0200

    compile

--- Source/DCSFlightpanels/Properties/AssemblyInfo.cs
+++ Source/DCSFlightpanels/Properties/AssemblyInfo.cs
@@ -49,5 +49,5 @@ using System.Windows;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("2.2.100.3832")]
-[assembly: AssemblyFileVersion("2.2.100.3832")]
+[assembly: AssemblyVersion("2.2.103.5666")]
+[assembly: AssemblyFileVersion("2.2.103.5666")]
