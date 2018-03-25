47859197f2bd499aeee59edbfdea1da698bba859
Jerker Dahlblom <jdahlblom@users.noreply.github.com>
2018-03-25 18:38:17 +0300

    SRS port changed to 9040

--- Source/DCSFlightpanels/Properties/Settings.Designer.cs
+++ Source/DCSFlightpanels/Properties/Settings.Designer.cs
@@ -12,7 +12,7 @@ namespace DCSFlightpanels.Properties {
     
     
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "15.5.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "15.6.0.0")]
     internal sealed partial class Settings : global::System.Configuration.ApplicationSettingsBase {
         
         private static Settings defaultInstance = ((Settings)(global::System.Configuration.ApplicationSettingsBase.Synchronized(new Settings())));
@@ -301,7 +301,7 @@ namespace DCSFlightpanels.Properties {
         
         [global::System.Configuration.UserScopedSettingAttribute()]
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [global::System.Configuration.DefaultSettingValueAttribute("9086")]
+        [global::System.Configuration.DefaultSettingValueAttribute("9040")]
         public int SRSPortTo {
             get {
                 return ((int)(this["SRSPortTo"]));
