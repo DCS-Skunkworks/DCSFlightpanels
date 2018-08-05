022b68fb09ba60767e0c8f6a2bba15a062409648
Jerker Dahlblom <jerker.dahlblom@gmail.com>
2018-08-05 18:49:39 +0300

    readme, compile

--- Source/DCSFlightpanels/Properties/AssemblyInfo.cs
+++ Source/DCSFlightpanels/Properties/AssemblyInfo.cs
@@ -49,5 +49,5 @@ using System.Windows;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("2.4.68.5100")]
-[assembly: AssemblyFileVersion("2.4.68.5100")]
+[assembly: AssemblyVersion("2.5.1.6736")]
+[assembly: AssemblyFileVersion("2.5.1.6736")]
--- Source/NonVisuals/Properties/AssemblyInfo.cs
+++ Source/NonVisuals/Properties/AssemblyInfo.cs
@@ -31,5 +31,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("2.1.851.5100")]
-[assembly: AssemblyFileVersion("2.1.851.5100")]
+[assembly: AssemblyVersion("2.1.853.6736")]
+[assembly: AssemblyFileVersion("2.1.853.6736")]
