a1bfc0ab73a5d67f58dbbe0124e26aea90d64725
Jerker Dahlblom <jdahlblom@users.noreply.github.com>
2018-11-11 14:42:20 +0200

    build

--- Source/DCS-BIOS/Properties/AssemblyInfo.cs
+++ Source/DCS-BIOS/Properties/AssemblyInfo.cs
@@ -31,5 +31,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("2.1.490.6060")]
-[assembly: AssemblyFileVersion("2.1.490.6060")]
+[assembly: AssemblyVersion("2.1.491.5289")]
+[assembly: AssemblyFileVersion("2.1.491.5289")]
