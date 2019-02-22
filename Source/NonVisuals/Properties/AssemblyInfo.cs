6fe2620b4063ffdf884e35f79ca929eb6219a8e9
Jerker Dahlblom <jdahlblom@users.noreply.github.com>
2019-02-22 15:36:42 +0200

    F-5 removal.

--- Source/NonVisuals/Properties/AssemblyInfo.cs
+++ Source/NonVisuals/Properties/AssemblyInfo.cs
@@ -32,6 +32,6 @@ using System.Runtime.InteropServices;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 
-[assembly: AssemblyVersion("2.1.898.6876")]
-[assembly: AssemblyFileVersion("2.1.898.6876")]
+[assembly: AssemblyVersion("2.1.899.5325")]
+[assembly: AssemblyFileVersion("2.1.899.5325")]
 
