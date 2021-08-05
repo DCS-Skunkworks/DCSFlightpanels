ac67d31a93d020e2376111f3819d18d44a4af2dd
Jerker Dahlblom <jdahlblom@users.noreply.github.com>
2021-08-05 16:55:47 +0300

    Added support for Stream Deck Original V2 PID 0x006D

--- Source/ClassLibraryCommon/CommonEnums.cs
+++ Source/ClassLibraryCommon/CommonEnums.cs
@@ -65,6 +65,8 @@ namespace ClassLibraryCommon
         StreamDeckMini = 0x0063,
         [Description("StreamDeck")]
         StreamDeck = 0x0060,
+        [Description("StreamDeck V2")]
+        StreamDeckV2 = 0x006D,
         [Description("StreamDeck XL")]
         StreamDeckXL = 0x006C
     }
--- Source/DCS-BIOS/Properties/AssemblyInfo.cs
+++ Source/DCS-BIOS/Properties/AssemblyInfo.cs
@@ -32,8 +32,8 @@ using System.Runtime.InteropServices;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 
-[assembly: AssemblyVersion("2.1.715.5505")]
-[assembly: AssemblyFileVersion("2.1.715.5505")]
+[assembly: AssemblyVersion("2.1.716.6062")]
+[assembly: AssemblyFileVersion("2.1.716.6062")]
 
 
 
