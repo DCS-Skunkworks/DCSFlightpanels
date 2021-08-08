8c3382810d2068293cba6de379572faf2514941b
Jerker Dahlblom <jdahlblom@users.noreply.github.com>
2021-08-08 15:44:38 +0300

    compile

--- Source/ClassLibraryCommon/Properties/AssemblyInfo.cs
+++ Source/ClassLibraryCommon/Properties/AssemblyInfo.cs
@@ -34,8 +34,8 @@ using System.Runtime.InteropServices;
 
 
 
-[assembly: AssemblyVersion("1.0.331.6590")]
-[assembly: AssemblyFileVersion("1.0.331.6590")]
+[assembly: AssemblyVersion("1.0.332.5639")]
+[assembly: AssemblyFileVersion("1.0.332.5639")]
 
 
 
--- Source/NonVisuals/Properties/AssemblyInfo.cs
+++ Source/NonVisuals/Properties/AssemblyInfo.cs
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 
-[assembly: AssemblyVersion("2.1.1110.6590")]
-[assembly: AssemblyFileVersion("2.1.1110.6590")]
+[assembly: AssemblyVersion("2.1.1111.5639")]
+[assembly: AssemblyFileVersion("2.1.1111.5639")]
