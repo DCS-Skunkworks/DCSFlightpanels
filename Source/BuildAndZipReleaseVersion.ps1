69d2b088b758ef073744b30fa04c022e4ff3e477
Mutex666 <steph.mutex@gmail.com>
2021-11-28 15:23:04 +0100

    Improve BuildAndZipReleaseVersion.ps1 script

--- Source/BuildAndZipReleaseVersion.ps1
+++ Source/BuildAndZipReleaseVersion.ps1
@@ -1,9 +1,12 @@
+#declaring & setting some variables
 $scriptPath = split-path -parent $MyInvocation.MyCommand.Definition
-$msBuildPath = 'C:\Program Files\Microsoft Visual Studio\2022\Community\Msbuild\Current\Bin\msbuild'
+$msBuildExePath = &"${env:ProgramFiles(x86)}\Microsoft Visual Studio\Installer\vswhere.exe" -latest -prerelease -products * -requires Microsoft.Component.MSBuild -find MSBuild\**\Bin\MSBuild.exe
+$destinationFolderPath = 'F:\My Drive\DCSFlightpanels Dev Test Folder\'
 
+Write-Host "Latest MsBuild path found is: $msBuildExePath"  -foregroundcolor "Green"
 Write-Host "Building release version " -foregroundcolor "Green"
 
-cmd.exe /c $msBuildPath /target:Build /property:Configuration=Release /property:Platform=x64 'DCSFlightpanels.local.sln'
+cmd.exe /c $msBuildExePath /verbosity:minimal /target:Build /property:Configuration=Release /property:Platform=x64 'DCSFlightpanels.local.sln'
 $buildLastExitCode = $LastExitCode
 
 Write-Host "Build LastExitCode: $buildLastExitCode" -foregroundcolor "Green"
@@ -16,10 +19,11 @@ if ( 0 -ne $buildLastExitCode )
 
 Write-Host "Getting file info" -foregroundcolor "Green"
 $file_version = (Get-Command $scriptPath\DCSFlightpanels\bin\x64\Release\dcsfp.exe).FileVersionInfo.FileVersion
-#Remove revision
+
+#Remove revision from file_version
 $file_version = $file_version.Remove($file_version.LastIndexOf('.'))
 
 Write-Host "Zip release folder & Copy to destination" -foregroundcolor "Green"
-Compress-Archive -Force -Path $scriptPath\DCSFlightpanels\bin\x64\Release\* -DestinationPath "F:\My Drive\DCSFlightpanels Dev Test Folder\DCSFlightpanels_x64_$file_version.zip"
+Compress-Archive -Force -Path $scriptPath\DCSFlightpanels\bin\x64\Release\* -DestinationPath $destinationFolderPath"\DCSFlightpanels_x64_$file_version.zip"
 
 Write-Host "Script end" -foregroundcolor "Green"
