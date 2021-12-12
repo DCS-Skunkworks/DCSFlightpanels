DCS-Skunkworks/DCSFlightpanels

commit 284ae0c47fb5f1a25fadc07f9d7d58d9a08a40e0

void SettingsCleared(object sender, PanelEventArgs e) removed from interface

Source/DCSFlightpanels/PanelUserControls/MultiPanelUserControl.xaml.cs
@@ -157,22 +157,6 @@
             }
         }
 
-        public void SettingsCleared(object sender, PanelEventArgs e)
-        {
-            try
-            {
-                if (e.PanelType == GamingPanelEnum.PZ70MultiPanel && _multiPanelPZ70.HIDInstanceId == e.HidInstance)
-                {
-                    ClearAll(false);
-                    ShowGraphicConfiguration();
-                }
-            }
-            catch (Exception ex)
-            {
-                Common.ShowErrorMessageBox( ex);
-            }
-        }
-
         private void ClearAll(bool clearAlsoProfile)
         {
             if (_textBoxBillsSet)
@@ -201,6 +185,7 @@
             {
                 return;
             }
+
             foreach (var textBox in Common.FindVisualChildren<PZ70TextBox>(this))
             {
                 if (textBox.Bill == null && !textBox.Equals(TextBoxLogPZ70))
@@ -210,11 +195,18 @@
                 _textBoxBillsSet = true;
             }
             _textBoxBillsSet = true;
+            
         }
 
         public void LedLightChanged(object sender, LedLightChangeEventArgs e) { }
 
-        public void PanelSettingsModified(object sender, PanelEventArgs e) { }
+        public void SettingsModified(object sender, PanelEventArgs e)
+        {
+            if (e.HidInstance == _multiPanelPZ70.HIDInstanceId)
+            {
+                Dispatcher?.BeginInvoke((Action)(ShowGraphicConfiguration));
+            }
+        }
         
         public void SettingsApplied(object sender, PanelEventArgs e)
         {
@@ -632,6 +624,10 @@
                         {
                             textBox.Bill.KeyPress = keyBinding.OSKeyPress;
                         }
+                        else
+                        {
+                            textBox.Bill.KeyPress = null;
+                        }
                     }
                 }
 
@@ -643,6 +639,10 @@
                         {
                             textBox.Bill.OSCommandObject = operatingSystemCommand.OSCommandObject;
                         }
+                        else
+                        {
+                            textBox.Bill.OSCommandObject = null;
+                        }
                 }
             
                 foreach (var dcsBiosBinding in _multiPanelPZ70.DCSBiosBindings)
@@ -651,6 +651,10 @@
                     if (dcsBiosBinding.DialPosition == _multiPanelPZ70.PZ70DialPosition && dcsBiosBinding.DCSBIOSInputs.Count > 0)
                     {
                         textBox.Bill.DCSBIOSBinding = dcsBiosBinding;
+                    }
+                    else
+                    {
+                        textBox.Bill.DCSBIOSBinding = null;
                     }
                 }
 
@@ -661,6 +665,10 @@
                     if (bipLink.DialPosition == _multiPanelPZ70.PZ70DialPosition && bipLink.BIPLights.Count > 0)
                     {
                         textBox.Bill.BipLink = bipLink;
+                    }
+                    else
+                    {
+                        textBox.Bill.BipLink = null;
                     }
                 }
 
@@ -675,6 +683,7 @@
                 {
                     ButtonLcdLower.Visibility = Visibility.Visible;
                 }
+
                 foreach (var dcsBiosBindingLCD in _multiPanelPZ70.LCDBindings)
                 {
                     if (dcsBiosBindingLCD.DialPosition == _multiPanelPZ70.PZ70DialPosition && dcsBiosBindingLCD.PZ70LCDPosition == PZ70LCDPosition.UpperLCD && dcsBiosBindingLCD.HasBinding)

Source/DCSFlightpanels/MainWindow.xaml.cs
@@ -961,24 +961,10 @@
                 Common.ShowErrorMessageBox(ex);
             }
         }
+        
+        public void LedLightChanged(object sender, LedLightChangeEventArgs e) { }
 
-        public void SettingsCleared(object sender, PanelEventArgs e)
-        {
-            try
-            {
-                SetWindowState();
-            }
-            catch (Exception ex)
-            {
-                Common.ShowErrorMessageBox(ex);
-            }
-        }
-
-        public void LedLightChanged(object sender, LedLightChangeEventArgs e)
-        {
-        }
-
-        public void PanelSettingsModified(object sender, PanelEventArgs e)
+        public void SettingsModified(object sender, PanelEventArgs e)
         {
             try
             {

Source/DCSFlightpanels/PanelUserControls/BackLitPanelUserControl.xaml.cs
@@ -106,22 +106,7 @@
         public void PanelBindingReadFromFile(object sender, PanelBindingReadFromFileEventArgs e){}
 
         public void SwitchesChanged(object sender, SwitchesChangedEventArgs e) { }
-
-        public void SettingsCleared(object sender, PanelEventArgs e)
-        {
-            try
-            {
-                if (e.PanelType == GamingPanelEnum.BackLitPanel && _backlitPanelBIP.HIDInstanceId == e.HidInstance)
-                {
-                    ShowGraphicConfiguration();
-                }
-            }
-            catch (Exception ex)
-            {
-                Common.ShowErrorMessageBox(ex);
-            }
-        }
-
+        
         public void LedLightChanged(object sender, LedLightChangeEventArgs e)
         {
             try
@@ -159,7 +144,7 @@
             }
         }
 
-        public void PanelSettingsModified(object sender, PanelEventArgs e)
+        public void SettingsModified(object sender, PanelEventArgs e)
         {
             try
             {
@@ -167,7 +152,11 @@
                 {
                     return;
                 }
-                Dispatcher?.BeginInvoke((Action)(ShowGraphicConfiguration));
+
+                if (_backlitPanelBIP.HIDInstanceId == e.HidInstance)
+                {
+                    Dispatcher?.BeginInvoke((Action)(ShowGraphicConfiguration));
+                }
             }
             catch (Exception ex)
             {

Source/DCSFlightpanels/PanelUserControls/FarmingPanelUserControl.xaml.cs
@@ -154,22 +154,6 @@
             }
         }
 
-        public void SettingsCleared(object sender, PanelEventArgs e)
-        {
-            try
-            {
-                if (e.PanelType == GamingPanelEnum.FarmingPanel && _farmingSidePanel.HIDInstanceId == e.HidInstance)
-                {
-                    ClearAll(false);
-                    ShowGraphicConfiguration();
-                }
-            }
-            catch (Exception ex)
-            {
-                Common.ShowErrorMessageBox(ex);
-            }
-        }
-
         public void LedLightChanged(object sender, LedLightChangeEventArgs e)
         {
             try
@@ -234,11 +218,14 @@
             }
         }
 
-        public void PanelSettingsModified(object sender, PanelEventArgs e)
+        public void SettingsModified(object sender, PanelEventArgs e)
         {
             try
             {
-                Dispatcher?.BeginInvoke((Action)(ShowGraphicConfiguration));
+                if (_farmingSidePanel.HIDInstanceId == e.HidInstance)
+                {
+                    Dispatcher?.BeginInvoke((Action)(ShowGraphicConfiguration));
+                }
             }
             catch (Exception ex)
             {
@@ -706,6 +693,10 @@
                     {
                         textBox.Bill.KeyPress = keyBinding.OSKeyPress;
                     }
+                    else
+                    {
+                        textBox.Bill.KeyPress = null;
+                    }
                 }
 
                 foreach (var operatingSystemCommand in _farmingSidePanel.OSCommandList)
@@ -714,6 +705,10 @@
                     if (operatingSystemCommand.OSCommandObject != null)
                     {
                         textBox.Bill.OSCommandObject = operatingSystemCommand.OSCommandObject;
+                    }
+                    else
+                    {
+                        textBox.Bill.OSCommandObject = null;
                     }
                 }
 
@@ -724,6 +719,10 @@
                     {
                         textBox.Bill.DCSBIOSBinding = dcsBiosBinding;
                     }
+                    else
+                    {
+                        textBox.Bill.DCSBIOSBinding = null;
+                    }
                 }
 
                 SetTextBoxBackgroundColors(Brushes.White); //Maybe we can remove this function and only retain the _textBoxBillsSet = true; ?
@@ -733,6 +732,10 @@
                     if (bipLink.BIPLights.Count > 0)
                     {
                         textBox.Bill.BipLink = bipLink;
+                    }
+                    else
+                    {
+                        textBox.Bill.BipLink = null;
                     }
                 }
             }

Source/DCSFlightpanels/PanelUserControls/StreamDeckUserControl.xaml.cs
@@ -204,20 +204,9 @@
             }
         }
 
-        public void SettingsCleared(object sender, PanelEventArgs e)
-        {
-            try
-            {
-            }
-            catch (Exception ex)
-            {
-                Common.ShowErrorMessageBox(ex);
-            }
-        }
-
         public void LedLightChanged(object sender, LedLightChangeEventArgs e) { }
 
-        public void PanelSettingsModified(object sender, PanelEventArgs e)
+        public void SettingsModified(object sender, PanelEventArgs e)
         {
             try
             {

Source/DCSFlightpanels/PanelUserControls/SwitchPanelPZ55UserControl.xaml
@@ -136,16 +136,6 @@
             </Grid>
         </GroupBox>
 
-
-        <GroupBox FontSize="10" Grid.Column="3" Grid.Row="2" Margin="0,0,0,137" Grid.RowSpan="4">
-            <StackPanel>
-
-                <TextBlock HorizontalAlignment="Stretch"  Text="Double-click in the key command boxes to erase them. Right-click to set timings." TextWrapping="Wrap" Background="#FFF5ECEC"></TextBlock>
-                <Button Content="Clear all" Height="23" HorizontalAlignment="Center" x:Name="ButtonClearAll" Width="75" Click="ButtonClearAllClick" Margin="0,20,0,0" />
-                <Button Content="DEV" Height="25" HorizontalAlignment="Center" Name="ButtonDEV" Width="75" Visibility="Collapsed" Click="ButtonDEV_OnClick" Margin="5,0" />
-            </StackPanel>
-        </GroupBox>
-
         <GroupBox Header="Log"  FontSize="10"  Grid.Column="4" Grid.Row="5" Grid.RowSpan="3" Margin="0,0.5,0,-1">
             <TextBox Name="TextBoxLogPZ55" VerticalScrollBarVisibility="Visible" VerticalAlignment="Stretch" TextWrapping="Wrap" />
         </GroupBox>

Source/DCSFlightpanels/PanelUserControls/SwitchPanelPZ55UserControl.xaml.cs
@@ -212,22 +212,6 @@
             }
         }
 
-        public void SettingsCleared(object sender, PanelEventArgs e)
-        {
-            try
-            {
-                if (e.PanelType == GamingPanelEnum.PZ55SwitchPanel && _switchPanelPZ55.HIDInstanceId == e.HidInstance)
-                {
-                    ClearAll(false);
-                    ShowGraphicConfiguration();
-                }
-            }
-            catch (Exception ex)
-            {
-                Common.ShowErrorMessageBox(ex);
-            }
-        }
-
         public void LedLightChanged(object sender, LedLightChangeEventArgs e)
         {
             try
@@ -364,11 +348,14 @@
             }
         }
 
-        public void PanelSettingsModified(object sender, PanelEventArgs e)
+        public void SettingsModified(object sender, PanelEventArgs e)
         {
             try
             {
-                Dispatcher?.BeginInvoke((Action)(ShowGraphicConfiguration));
+                if (_switchPanelPZ55.HIDInstanceId == e.HidInstance)
+                {
+                    Dispatcher?.BeginInvoke((Action)(ShowGraphicConfiguration));
+                }
             }
             catch (Exception ex)
             {
@@ -819,6 +806,10 @@
                     {
                         textBox.Bill.KeyPress = keyBinding.OSKeyPress;
                     }
+                    else
+                    {
+                        textBox.Bill.KeyPress = null;
+                    }
                 }
 
                 foreach (var operatingSystemCommand in _switchPanelPZ55.OSCommandList)
@@ -827,6 +818,10 @@
                     if (operatingSystemCommand.OSCommandObject != null)
                     {
                         textBox.Bill.OSCommandObject = operatingSystemCommand.OSCommandObject;
+                    }
+                    else
+                    {
+                        textBox.Bill.OSCommandObject = null;
                     }
                 }
 
@@ -837,6 +832,10 @@
                     {
                         textBox.Bill.DCSBIOSBinding = dcsBiosBinding;
                     }
+                    else
+                    {
+                        textBox.Bill.DCSBIOSBinding = null;
+                    }
                 }
 
                 SetTextBoxBackgroundColors(Brushes.White); //Maybe we can remove this function and only retain the _textBoxBillsSet = true; ?
@@ -846,6 +845,10 @@
                     if (bipLinkPZ55.BIPLights.Count > 0)
                     {
                         textBox.Bill.BipLink = bipLinkPZ55;
+                    }
+                    else
+                    {
+                        textBox.Bill.BipLink = null;
                     }
                 }
 

Source/DCSFlightpanels/PanelUserControls/TPMPanelUserControl.xaml.cs
@@ -155,34 +155,13 @@
                 Common.ShowErrorMessageBox(ex);
             }
         }
-
-        public void SettingsCleared(object sender, PanelEventArgs e)
-        {
-            try
-            {
-                if (e.PanelType == GamingPanelEnum.TPM && _tpmPanel.HIDInstanceId == e.HidInstance)
-                {
-                    ClearAll(false);
-                    ShowGraphicConfiguration();
-                }
-            }
-            catch (Exception ex)
-            {
-                Common.ShowErrorMessageBox(ex);
-            }
-        }
         
-        public void DeviceAttached(object sender, PanelEventArgs e)
-        {
-        }
+        
+        public void DeviceAttached(object sender, PanelEventArgs e) { }
 
-        public void LedLightChanged(object sender, LedLightChangeEventArgs e)
-        {
-        }
+        public void LedLightChanged(object sender, LedLightChangeEventArgs e) { }
 
-        public void DeviceDetached(object sender, PanelEventArgs e)
-        {
-        }
+        public void DeviceDetached(object sender, PanelEventArgs e) { }
 
         public void SettingsApplied(object sender, PanelEventArgs e)
         {
@@ -200,11 +179,14 @@
             }
         }
 
-        public void PanelSettingsModified(object sender, PanelEventArgs e)
+        public void SettingsModified(object sender, PanelEventArgs e)
         {
             try
             {
-                Dispatcher?.BeginInvoke((Action)(ShowGraphicConfiguration));
+                if (_tpmPanel.HIDInstanceId == e.HidInstance)
+                {
+                    Dispatcher?.BeginInvoke((Action)(ShowGraphicConfiguration));
+                }
             }
             catch (Exception ex)
             {
@@ -538,6 +520,10 @@
                     {
                         textBox.Bill.KeyPress = keyBinding.OSKeyPress;
                     }
+                    else
+                    {
+                        textBox.Bill.KeyPress = null;
+                    }
                 }
 
                 foreach (var operatingSystemCommand in _tpmPanel.OSCommandHashSet)
@@ -546,6 +532,10 @@
                     if (operatingSystemCommand.OSCommandObject != null)
                     {
                         textBox.Bill.OSCommandObject = operatingSystemCommand.OSCommandObject;
+                    }
+                    else
+                    {
+                        textBox.Bill.OSCommandObject = null;
                     }
                 }
 
@@ -556,6 +546,10 @@
                     {
                         textBox.Bill.DCSBIOSBinding = dcsBiosBinding;
                     }
+                    else
+                    {
+                        textBox.Bill.DCSBIOSBinding = null;
+                    }
                 }
 
                 foreach (var bipLink in _tpmPanel.BipLinkHashSet)
@@ -564,6 +558,10 @@
                     if (bipLink.BIPLights.Count > 0)
                     {
                         textBox.Bill.BipLink = bipLink;
+                    }
+                    else
+                    {
+                        textBox.Bill.BipLink = null;
                     }
                 }
             }

commit 90e17f6bc7e58e62ed9cca550a79fd378d5b3acf

Fix bug introduced in 284ae0c4. Only CRC bindings where showing

Source/DCSFlightpanels/PanelUserControls/MultiPanelUserControl.xaml.cs
@@ -617,10 +617,6 @@
                         {
                             textBox.Bill.KeyPress = keyBinding.OSKeyPress;
                         }
-                        else
-                        {
-                            textBox.Bill.KeyPress = null;
-                        }
                     }
                 }
 
@@ -632,10 +628,6 @@
                         {
                             textBox.Bill.OSCommandObject = operatingSystemCommand.OSCommandObject;
                         }
-                        else
-                        {
-                            textBox.Bill.OSCommandObject = null;
-                        }
                 }
 
                 foreach (var dcsBiosBinding in _multiPanelPZ70.DCSBiosBindings)
@@ -644,10 +636,6 @@
                     if (dcsBiosBinding.DialPosition == _multiPanelPZ70.PZ70DialPosition && dcsBiosBinding.DCSBIOSInputs.Count > 0)
                     {
                         textBox.Bill.DCSBIOSBinding = dcsBiosBinding;
-                    }
-                    else
-                    {
-                        textBox.Bill.DCSBIOSBinding = null;
                     }
                 }
 
@@ -658,10 +646,6 @@
                     if (bipLink.DialPosition == _multiPanelPZ70.PZ70DialPosition && bipLink.BIPLights.Count > 0)
                     {
                         textBox.Bill.BipLink = bipLink;
-                    }
-                    else
-                    {
-                        textBox.Bill.BipLink = null;
                     }
                 }
 