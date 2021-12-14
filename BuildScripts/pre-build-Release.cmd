ca8567f913610bd47cb3b0ad9d15b8a60b6d4e66
Mutex666 <steph.mutex@gmail.com>
2021-12-14 17:13:06 +0100

    Changes for publish

--- BuildScripts/pre-build-Release.cmd
+++ BuildScripts/pre-build-Release.cmd
@@ -1,2 +1,2 @@
-.\UpdateVersion.exe -b Increment -r ddHHmm -i "%1Properties\AssemblyInfo.cs" -o "%1Properties\AssemblyInfo.cs"
-.\UpdateVersion.exe -v File -b Increment -r ddHHmm -i "%1Properties\AssemblyInfo.cs" -o "%1Properties\AssemblyInfo.cs"
+.\UpdateVersion.exe -b Increment -r ddHHmm -i "%1\Properties\AssemblyInfo.cs" -o "%1\Properties\AssemblyInfo.cs"
+.\UpdateVersion.exe -v File -b Increment -r ddHHmm -i "%1\Properties\AssemblyInfo.cs" -o "%1\Properties\AssemblyInfo.cs"
