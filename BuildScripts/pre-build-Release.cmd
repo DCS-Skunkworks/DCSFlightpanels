1483e22788f5cac20e2da0f88a990e872129135e
Mutex666 <steph.mutex@gmail.com>
2021-12-18 14:04:40 +0100

    Remove BuildScripts folder

--- BuildScripts/pre-build-Release.cmd
+++
@@ -1,2 +0,0 @@
-.\UpdateVersion.exe -b Increment -r ddHHmm -i "%1\Properties\AssemblyInfo.cs" -o "%1\Properties\AssemblyInfo.cs"
-.\UpdateVersion.exe -v File -b Increment -r ddHHmm -i "%1\Properties\AssemblyInfo.cs" -o "%1\Properties\AssemblyInfo.cs"
