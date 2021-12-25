6dd529e3c74d8547220fe999223a86ed4e3debf0
Mutex666 <steph.mutex@gmail.com>
2021-12-25 10:40:29 +0100

    Rename variable in GetRelativePath()

--- Source/ClassLibraryCommon/Common.cs
+++ Source/ClassLibraryCommon/Common.cs
@@ -235,13 +235,12 @@
 
         public static string GetRelativePath(string relativeTo, string path)
         {
-            var uri = new Uri(relativeTo);
-            var rel = Uri.UnescapeDataString(uri.MakeRelativeUri(new Uri(path)).ToString()).Replace(Path.AltDirectorySeparatorChar, Path.DirectorySeparatorChar);
-            if (rel.Contains(Path.DirectorySeparatorChar.ToString()) == false)
+            var uriRelativeTo = new Uri(relativeTo);
+            var rel = Uri.UnescapeDataString(uriRelativeTo.MakeRelativeUri(new Uri(path)).ToString()).Replace(Path.AltDirectorySeparatorChar, Path.DirectorySeparatorChar);
+            if (!rel.Contains(Path.DirectorySeparatorChar.ToString()))
             {
                 rel = $".{ Path.DirectorySeparatorChar }{ rel }";
             }
-
             return rel;
         }
 
