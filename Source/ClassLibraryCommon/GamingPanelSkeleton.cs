4564b7ab25fee84a63485b6cb79049e85e185ceb
Mutex666 <steph.mutex@gmail.com>
2021-12-26 17:44:12 +0100

    Remove SerialNumber from GamingPanelSkeleton, change ToString()

--- Source/ClassLibraryCommon/GamingPanelSkeleton.cs
+++ Source/ClassLibraryCommon/GamingPanelSkeleton.cs
@@ -1,13 +1,10 @@
 ﻿namespace ClassLibraryCommon
 {
-    using System.Text;
-
     public class GamingPanelSkeleton
     {
         public GamingPanelEnum GamingPanelType { get; set; } = GamingPanelEnum.Unknown;
         public int VendorId { get; set; }
         public int ProductId { get; set; }
-        public string SerialNumber { get; set; }
 
         public GamingPanelSkeleton(GamingPanelVendorEnum gamingPanelVendor, GamingPanelEnum gamingPanelsEnum)
         {
@@ -16,24 +13,9 @@
             ProductId = (int)gamingPanelsEnum;
         }
 
-        public GamingPanelSkeleton(GamingPanelVendorEnum gamingPanelVendor, GamingPanelEnum gamingPanelsEnum, string serialNumber)
-        {
-            GamingPanelType = gamingPanelsEnum;
-            VendorId = (int)gamingPanelVendor;
-            ProductId = (int)gamingPanelsEnum;
-            SerialNumber = serialNumber;
-        }
-
-        public bool HasSerialNumber()
-        {
-            return !string.IsNullOrEmpty(SerialNumber) && !SerialNumber.Equals("0");
-        }
-
         public override string ToString()
         {
-            var stringBuilder = new StringBuilder();
-            stringBuilder.Append("Skeleton : ").Append(GamingPanelType).Append(" ").Append(VendorId).Append(" ").Append(ProductId);
-            return stringBuilder.ToString();
+            return $"Skeleton : {GamingPanelType} {VendorId} {ProductId}";
         }
     }
 }
