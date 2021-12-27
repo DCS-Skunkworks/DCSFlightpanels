226cc9cd872143ecfe93f9063474bb21da168e2d
Mutex666 <steph.mutex@gmail.com>
2021-12-27 15:36:54 +0100

    Add DCSBIOSInputObject Consume() Tests

---
+++ Source/Tests/DcsBios/DCSBIOSInputObjectTests.cs
@@ -0,0 +1,35 @@
+﻿using DCS_BIOS;
+using DCS_BIOS.Json;
+using Xunit;
+
+namespace Tests.DcsBios
+{
+    public class DCSBIOSInputObjectTests
+    {
+        [Theory]
+        [InlineData("fixed_step", DCSBIOSInputType.FIXED_STEP)]
+        [InlineData("set_state", DCSBIOSInputType.SET_STATE)]
+        [InlineData("action", DCSBIOSInputType.ACTION)]
+        [InlineData("variable_step", DCSBIOSInputType.VARIABLE_STEP)]
+        public void Cosume_ShouldSet_4_Properties(string givenControlInterface, DCSBIOSInputType expectedDCSBIOSInputType)
+        {
+            string controlId = "ThisIsAControlId";
+
+            DCSBIOSInputObject inputObject = new();
+            DCSBIOSControlInput controlInput = new()
+            {
+                Description = "Test",
+                ControlInterface = givenControlInterface,
+                MaxValue = 666,
+                Argument = "ThisIsAnArgument"
+            };
+            inputObject.Consume(controlId, controlInput);
+
+            Assert.Equal(controlId, inputObject.ControlId);
+            Assert.Equal(controlInput.Description, inputObject.Description);
+            Assert.Equal(expectedDCSBIOSInputType, inputObject.Interface);
+            Assert.Equal(controlInput.Argument, inputObject.SpecifiedActionArgument);
+        }
+
+    }
+}
