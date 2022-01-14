3c4f482ac7afbc647ea9a059a5ef4b039a6cae2c
Jerker Dahlblom <jerker.dahlblom@gmail.com>
2022-01-14 18:48:19 +0200

    comments

--- Source/NonVisuals/EventArgs/PanelEventArgs.cs
+++ Source/NonVisuals/EventArgs/PanelEventArgs.cs
@@ -17,12 +17,33 @@
 
     public enum PanelEventType
     {
+        /// <summary>
+        /// Triggered when a panel is re-inserted and has the same HID ID as before
+        /// </summary>
         Attached,
+        /// <summary>
+        /// Triggered when panel removed
+        /// </summary>
         Detached,
+        /// <summary>
+        /// Triggered when DCSFP has automatically found a panel
+        /// </summary>
         Found,
-        ManuallyFound, // This is when the user explicitly searches for panels. Difference is that in this event chain [AllPanelsFound] will not be sent.
+        /// <summary>
+        /// This is when the user explicitly searches for panels. Difference is that in this event chain [AllPanelsFound] will not be sent.
+        /// </summary>
+        ManuallyFound, 
+        /// <summary>
+        /// Triggered when a Panel has been created
+        /// </summary>
         Created,
+        /// <summary>
+        /// Triggered when a Panel has been disposed
+        /// </summary>
         Disposed,
+        /// <summary>
+        /// Triggered when automatic search for panel has ended
+        /// </summary>
         AllPanelsFound
     }
 }
