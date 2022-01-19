d4919029f84b472b3b6aa2ee2d27a2a8db37648c
Jerker Dahlblom <jerker.dahlblom@gmail.com>
2022-01-19 20:03:25 +0200

    added bit info for comms with saitek panels.

--- Source/NonVisuals/Saitek/Switches/SwitchPanelKey.cs
+++ Source/NonVisuals/Saitek/Switches/SwitchPanelKey.cs
@@ -103,3 +103,54 @@
         }
     }
 }
+/*
+    Byte #1
+    00000000
+    ||||||||_ SWITCHKEY_MASTER_BAT
+    |||||||_ SWITCHKEY_MASTER_ALT
+    ||||||_ SWITCHKEY_AVIONICS_MASTER
+    |||||_ SWITCHKEY_FUEL_PUMP
+    ||||_ SWITCHKEY_DE_ICE
+    |||_ SWITCHKEY_PITOT_HEAT
+    ||_ SWITCHKEY_CLOSE_COWL ** ~
+    |_ SWITCHKEY_LIGHTS_PANEL
+
+    Byte #2 
+    00000000
+    ||||||||_ SWITCHKEY_LIGHTS_BEACON
+    |||||||_ SWITCHKEY_LIGHTS_NAV
+    ||||||_ SWITCHKEY_LIGHTS_STROBE
+    |||||_ SWITCHKEY_LIGHTS_TAXI
+    ||||_ SWITCHKEY_LIGHTS_LANDING
+    |||_ KNOB_ENGINE_OFF
+    ||_ KNOB_ENGINE_RIGHT
+    |_ KNOB_ENGINE_LEFT
+
+    Byte #3
+    00000000
+    ||||||||_ KNOB_ENGINE_BOTH
+    |||||||_ KNOB_ENGINE_START
+    ||||||_ LEVER_GEAR_UP
+    |||||_ LEVER_GEAR_DOWN
+    ||||_ 
+    |||_ 
+    ||_ 
+    |_
+
+
+
+    LED Byte:
+    * 00000000 0x0 ALL DARK
+    * 
+    * 00000001 0x1 UP GREEN
+    * 00001000 0x8 UP RED
+    * 00001001 0x9 UP YELLOW
+    * 
+    * 00000010 0x2 LEFT GREEN
+    * 00010000 0x10 LEFT RED
+    * 00010010 0x12 LEFT YELLOW
+    * 
+    * 00100000 0x20 RIGHT RED
+    * 00000100 0x4 RIGHT GREEN
+    * 00100100 0x24 RIGHT YELLOW
+ */
