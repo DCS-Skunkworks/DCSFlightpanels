e2c9f16bce49786bf4c70fd4e026e97aa841f8f1
Cerppo <cerpo.gaming@gmail.com>
2022-10-01 16:21:20 +0200

    Feat: Added support for RIO CAP panel and keys minor changes to BaseCDU ( baseColor )

---
+++ Source/DCSFlightpanels/PanelUserControls/PreProgrammed/Cdu737UserControlF14.xaml.cs
@@ -0,0 +1,116 @@
+﻿namespace DCSFlightpanels.PanelUserControls.PreProgrammed
+{
+    using System.Windows;
+    using System.Windows.Controls;
+    using NonVisuals;
+    using NonVisuals.EventArgs;
+    using NonVisuals.Interfaces;
+
+    using NonVisuals.CockpitMaster.Preprogrammed;
+    using DCSFlightpanels.Interfaces;
+    using System;
+    using DCSFlightpanels.CustomControls;
+
+    /// <summary>
+    /// Interaction logic for Cdu737UserControlF14.xaml
+    /// </summary>
+    /// 
+    public partial class Cdu737UserControlF14 : UserControlBase,
+        IGamingPanelListener,
+        IGamingPanelUserControl
+    {
+        private readonly CDU737PanelF14 _CDU737PanelF14;
+
+        public Cdu737UserControlF14(HIDSkeleton hidSkeleton, TabItem parentTabItem)
+        {
+            InitializeComponent();
+            ParentTabItem = parentTabItem;
+
+            _CDU737PanelF14 = new CDU737PanelF14(hidSkeleton);
+            //_HIDSkeleton = hidSkeleton;
+            AppEventHandler.AttachGamingPanelListener(this);
+
+            HideAllImages();
+
+        }
+        private bool _disposed;
+        // Protected implementation of Dispose pattern.
+        protected override void Dispose(bool disposing)
+        {
+            if (!_disposed)
+            {
+                if (disposing)
+                {
+                    _CDU737PanelF14.Dispose();
+                    AppEventHandler.DetachGamingPanelListener(this);
+
+                }
+                _disposed = true;
+            }
+
+            // Call base class implementation.
+            base.Dispose(disposing);
+        }
+
+        private bool _once = true;
+
+        private void CDU737UserControl_OnLoaded(object sender, RoutedEventArgs e)
+        {
+            UserControlLoaded = true;
+
+            if (_once)
+            {
+                _once = false;
+            }
+        }
+
+        public override GamingPanel GetGamingPanel()
+        {
+            return _CDU737PanelF14;
+        }
+
+
+        public void ProfileEvent(object sender, ProfileEventArgs e)
+        {
+
+        }
+
+        public void SettingsApplied(object sender, PanelInfoArgs e)
+        {
+
+        }
+
+        public void SettingsModified(object sender, PanelInfoArgs e)
+        {
+
+        }
+
+        public void SwitchesChanged(object sender, SwitchesChangedEventArgs e)
+        {
+            string[] lines = _CDU737PanelF14.CDULines;
+            Dispatcher?.BeginInvoke(
+            (Action)
+            (() =>
+            {
+                CDU737UserControl.displayLines(lines, 10);
+            }
+            ));
+        }
+
+        public void UpdatesHasBeenMissed(object sender, DCSBIOSUpdatesMissedEventArgs e)
+        {
+
+        }
+        private void HideAllImages()
+        {
+
+        }
+
+        public string GetName()
+        {
+            return GetType().Name;
+        }
+
+    }
+}
+
