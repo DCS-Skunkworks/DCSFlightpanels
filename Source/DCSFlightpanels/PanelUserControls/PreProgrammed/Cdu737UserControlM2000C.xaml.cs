321fe888683da388d9e8771c2df5a1be0ae15648
Cerppo <49897630+Cerppo@users.noreply.github.com>
2022-10-09 15:40:54 +0200

    Feat: PCN Keyboard Mapping (#222)

---
+++ Source/DCSFlightpanels/PanelUserControls/PreProgrammed/Cdu737UserControlM2000C.xaml.cs
@@ -0,0 +1,115 @@
+﻿namespace DCSFlightpanels.PanelUserControls.PreProgrammed
+{
+    using System.Windows;
+    using System.Windows.Controls;
+    using NonVisuals;
+    using NonVisuals.EventArgs;
+    using NonVisuals.Interfaces;
+
+    using NonVisuals.CockpitMaster.Preprogrammed;
+    using DCSFlightpanels.Interfaces;
+    using System;
+    using DCSFlightpanels.CustomControls;
+
+    /// <summary>
+    /// Interaction logic for Cdu737UserControlM2000C.xaml
+    /// </summary>
+    /// 
+    public partial class Cdu737UserControlM2000C : UserControlBase,
+        IGamingPanelListener,
+        IGamingPanelUserControl
+    {
+        private readonly CDU737PanelM2000C _CDU737PanelM2000C;
+
+        public Cdu737UserControlM2000C(HIDSkeleton hidSkeleton, TabItem parentTabItem)
+        {
+            InitializeComponent();
+            ParentTabItem = parentTabItem;
+
+            _CDU737PanelM2000C = new CDU737PanelM2000C(hidSkeleton);
+            AppEventHandler.AttachGamingPanelListener(this);
+
+            HideAllImages();
+
+        }
+        private bool _disposed;
+        // Protected implementation of Dispose pattern.
+        protected override void Dispose(bool disposing)
+        {
+            if (!_disposed)
+            {
+                if (disposing)
+                {
+                    _CDU737PanelM2000C.Dispose();
+                    AppEventHandler.DetachGamingPanelListener(this);
+
+                }
+                _disposed = true;
+            }
+
+            // Call base class implementation.
+            base.Dispose(disposing);
+        }
+
+        private bool _once = true;
+
+        private void CDU737UserControl_OnLoaded(object sender, RoutedEventArgs e)
+        {
+            UserControlLoaded = true;
+
+            if (_once)
+            {
+                _once = false;
+            }
+        }
+
+        public override GamingPanel GetGamingPanel()
+        {
+            return _CDU737PanelM2000C;
+        }
+
+
+        public void ProfileEvent(object sender, ProfileEventArgs e)
+        {
+
+        }
+
+        public void SettingsApplied(object sender, PanelInfoArgs e)
+        {
+
+        }
+
+        public void SettingsModified(object sender, PanelInfoArgs e)
+        {
+
+        }
+
+        public void SwitchesChanged(object sender, SwitchesChangedEventArgs e)
+        {
+            string[] lines = _CDU737PanelM2000C.CDULines;
+            Dispatcher?.BeginInvoke(
+            (Action)
+            (() =>
+            {
+                CDU737UserControl.displayLines(lines, 10);
+            }
+            ));
+        }
+
+        public void UpdatesHasBeenMissed(object sender, DCSBIOSUpdatesMissedEventArgs e)
+        {
+
+        }
+        private void HideAllImages()
+        {
+
+        }
+
+        public string GetName()
+        {
+            return GetType().Name;
+        }
+
+    }
+}
+
