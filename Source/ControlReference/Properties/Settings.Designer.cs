9497f110cc9e80509c483d8d5c8ecd00646415f8
Jerker Dahlblom <jdahlblom@users.noreply.github.com>
2023-02-16 18:53:47 +0200

    ctrl-ref topmost added (#297)

--- Source/ControlReference/Properties/Settings.Designer.cs
+++ Source/ControlReference/Properties/Settings.Designer.cs
@@ -130,5 +130,17 @@ namespace ControlReference.Properties {
                 this["MainWindowLeft"] = value;
             }
         }
+        
+        [global::System.Configuration.UserScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.DefaultSettingValueAttribute("True")]
+        public bool AlwaysOnTop {
+            get {
+                return ((bool)(this["AlwaysOnTop"]));
+            }
+            set {
+                this["AlwaysOnTop"] = value;
+            }
+        }
     }
 }
