1d5ec09c8390a269afab2dcabf0d4297d22005fb
Jerker Dahlblom <jdahlblom@users.noreply.github.com>
2024-02-12 10:13:47 +0200

    More tests

--- src/Tests/DcsBios/DCSBIOSInputTests.cs
+++ src/Tests/DcsBios/DCSBIOSInputTests.cs
@@ -73,5 +73,35 @@ namespace DCSFPTests.DcsBios
             Assert.Equal(dcsbiosInput.SelectedDCSBIOSInterface.Interface, dcsbiosInputType);
         }
 
+        [Theory]
+        [InlineData(@"%USERPROFILE%\Saved Games\DCS\Scripts\DCS-BIOS\doc\json", 5, "OXY_SUPPLY", DCSBIOSInputType.ACTION, "Oxygen Supply On/Off")] // A-10C Thunderbolt/II
+        [InlineData(@"%USERPROFILE%\Saved Games\DCS\Scripts\DCS-BIOS\doc\json", 5, "FLAPS_SWITCH", DCSBIOSInputType.FIXED_STEP, "Flaps Setting DN - MVR - UP")]
+        [InlineData(@"%USERPROFILE%\Saved Games\DCS\Scripts\DCS-BIOS\doc\json", 5, "UFC_1", DCSBIOSInputType.SET_STATE, "1")]
+        [InlineData(@"%USERPROFILE%\Saved Games\DCS\Scripts\DCS-BIOS\doc\json", 5, "ENGINE_THROTTLE_FRICTION", DCSBIOSInputType.VARIABLE_STEP, "Friction Control")]
+        [InlineData(@"%USERPROFILE%\Saved Games\DCS\Scripts\DCS-BIOS\doc\json", 38, "UHF_ARC51", DCSBIOSInputType.SET_STRING, "UHF ARC-51 Radio")] // UH-1H Huey
+        public void TestDCSBIOSInputGetDescriptionForInterface(string jsonPath, int aircraftId, string dcsbiosControlId, DCSBIOSInputType dcsbiosInputType, string description)
+        {
+            // !! Invalid path logs to error log but doesn't throw exception to the outside
+            jsonPath = Environment.ExpandEnvironmentVariables(jsonPath);
+            if (!Directory.Exists(jsonPath))
+            {
+                //No need to test as dcs-bios isn't installed
+                return;
+            }
+            Common.ResetEmulationModesFlag();
+
+            DCSAircraft.FillModulesListFromDcsBios(jsonPath, true);
+            var dcsAircraft = DCSAircraft.GetAircraft(aircraftId);
+
+            DCSBIOSControlLocator.DCSAircraft = dcsAircraft;
+            DCSBIOSControlLocator.JSONDirectory = jsonPath;
+
+            var dcsbiosControl = DCSBIOSControlLocator.GetControl(dcsbiosControlId);
+            var dcsbiosInput = new DCSBIOSInput();
+            dcsbiosInput.Consume(dcsbiosControl);
+
+            Assert.Equal(dcsbiosInput.ControlDescription, description);
+        }
+
     }
 }
