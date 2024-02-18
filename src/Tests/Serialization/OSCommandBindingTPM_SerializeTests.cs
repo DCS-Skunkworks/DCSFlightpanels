011a2b777f9156f7f1974d96d530d002ce309452
Mutex666 <67550369+Mutex666@users.noreply.github.com>
2024-02-18 07:11:46 +0100

    Add OSCommandBindingTPM_SerializeTests

---
+++ src/Tests/Serialization/OSCommandBindingTPM_SerializeTests.cs
@@ -0,0 +1,45 @@
+﻿using DCSFPTests.Serialization.Common;
+using MEF;
+using Newtonsoft.Json;
+using NonVisuals.BindingClasses.OSCommand;
+using Xunit;
+
+
+namespace DCSFPTests.Serialization {
+    public class OSCommandBindingTPM_SerializeTests {
+        [Fact]
+        public static void OSCommandBindingTPM_ShouldBeSerializable() {
+            OSCommandBindingTPM s = GetObject();
+
+            string serializedObj = JsonConvert.SerializeObject(s, Formatting.Indented, JSonSettings.JsonDefaultSettings);
+            OSCommandBindingTPM d = JsonConvert.DeserializeObject<OSCommandBindingTPM>(serializedObj, JSonSettings.JsonDefaultSettings);
+
+            Assert.Equal(s.TPMSwitch, d.TPMSwitch);
+            Assert.Equal(s.WhenTurnedOn, d.WhenTurnedOn);
+            DeepAssert.Equal(s.OSCommandObject, d.OSCommandObject);
+
+            //Ignored :
+            //HasSequence
+            //IsSequenced
+
+            RepositorySerialized repo = new();
+            //Save sample file in project (use it only once)
+            //repo.SaveSerializedObjectToFile(s.GetType(), serializedObj);
+
+            OSCommandBindingTPM deseralizedObjFromFile = JsonConvert.DeserializeObject<OSCommandBindingTPM>(repo.GetSerializedObjectString(s.GetType()), JSonSettings.JsonDefaultSettings);
+
+            Assert.Equal(s.TPMSwitch, deseralizedObjFromFile.TPMSwitch);
+            Assert.Equal(s.WhenTurnedOn, deseralizedObjFromFile.WhenTurnedOn);
+            DeepAssert.Equal(s.OSCommandObject, d.OSCommandObject);
+        }
+
+        private static OSCommandBindingTPM GetObject(int instanceNbr = 1) {
+            return new()
+            {
+                TPMSwitch = KeyBindingTPM_SerializeTests.GetTPMPanelSwitchesFromInstance(instanceNbr + 6),
+                WhenTurnedOn = true,
+                OSCommandObject = OSCommand_SerializeTests.GetObject(instanceNbr + 2)
+            };
+        }
+    }
+}
