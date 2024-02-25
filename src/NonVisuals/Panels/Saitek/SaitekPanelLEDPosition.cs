46147c7a6f3c6bdcfaec7ed9f4ba05f8e608bfee
Mutex666 <67550369+Mutex666@users.noreply.github.com>
2024-02-25 10:06:42 +0100

    Split DcsOutputAndColorBinding to own classes

---
+++ src/NonVisuals/Panels/Saitek/SaitekPanelLEDPosition.cs
@@ -0,0 +1,31 @@
+﻿namespace NonVisuals.Panels.Saitek {
+    using System;
+    using ClassLibraryCommon;
+
+    [SerializeCritical]
+    public class SaitekPanelLEDPosition
+    {
+        private Enum _position;
+
+        public SaitekPanelLEDPosition(Enum position)
+        {
+            _position = position;
+        }
+
+        public Enum GetPosition()
+        {
+            return _position;
+        }
+
+        public void SetPosition(Enum position)
+        {
+            _position = position;
+        }
+
+        public Enum Position
+        {
+            get => _position;
+            set => _position = value;
+        }
+    }
+}
