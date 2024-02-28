768b1ce453becad3c3b8222e4ae1c03c5c60eb47
Mutex666 <67550369+Mutex666@users.noreply.github.com>
2024-02-28 10:44:37 +0100

    [Serialize Tests] DcsOutputAndColorBinding serialize tests (#489)
    * Split DcsOutputAndColorBinding to own classes
    
    * Add DcsOutputAndColorBindingPZ55 serialize test (wip)
    
    * Test enum convert
    
    * Change Enum to int in SaitekPanelLEDPosition
    
    * Add DcsOutputAndColorBindingPZ55_SerializeTests
    
    * Add DcsOutputAndColorBindingBIP_SerializeTests
    
    ---------
    
    Co-authored-by: Jerker Dahlblom <jdahlblom@users.noreply.github.com>

---
+++ src/NonVisuals/Panels/Saitek/DcsOutputAndColorBindingBIP.cs
@@ -0,0 +1,49 @@
+﻿namespace NonVisuals.Panels.Saitek {
+    using System;
+    using System.Text;
+    using ClassLibraryCommon;
+    using DCS_BIOS.Serialized;
+    using Panels;
+
+    [SerializeCriticalCustom]
+    public class DcsOutputAndColorBindingBIP : DcsOutputAndColorBinding
+    {
+        public override void ImportSettings(string settings)
+        {
+            if (string.IsNullOrEmpty(settings))
+            {
+                return;
+            }
+
+            // PanelBIP{Position_1_4|GREEN|DCSBiosOutput{INTEGER_TYPE|Equals|0x14be|0x4000|14|1}}\o/\\?\hid#vid_06a3&pid_0b4e#9&1f079469&0&0000#{4d1e55b2-f16f-11cf-88cb-001111000030}
+            settings = settings.Split(Separator, StringSplitOptions.RemoveEmptyEntries)[0];
+
+            // PanelBIP{Position_1_4|GREEN|DCSBiosOutput{INTEGER_TYPE|Equals|0x14be|0x4000|14|1}}
+            settings = settings.Substring(settings.IndexOf("{", StringComparison.InvariantCulture) + 1);
+
+            // Position_1_4|GREEN|DCSBiosOutput{INTEGER_TYPE|Equals|0x14be|0x4000|14|1}}
+            settings = settings.Substring(0, settings.Length - 1);
+
+            // Position_1_4|GREEN|DCSBiosOutput{INTEGER_TYPE|Equals|0x14be|0x4000|14|1}
+            var dcsBiosOutputString = settings.Substring(settings.IndexOf("DCSBiosOutput{", StringComparison.InvariantCulture));
+
+            // DCSBiosOutput{INTEGER_TYPE|Equals|0x14be|0x4000|14|1}
+            settings = settings.Remove(settings.IndexOf("DCSBiosOutput{", StringComparison.InvariantCulture));
+
+            // Position_1_4|GREEN|
+            var settingsArray = settings.Split(new[] { '|' }, StringSplitOptions.RemoveEmptyEntries);
+            SaitekLEDPosition = new SaitekPanelLEDPosition((int)Enum.Parse(typeof(BIPLedPositionEnum), settingsArray[0]));
+            LEDColor = (PanelLEDColor)Enum.Parse(typeof(PanelLEDColor), settingsArray[1]);
+            DCSBiosOutputLED = new DCSBIOSOutput();
+            DCSBiosOutputLED.ImportString(dcsBiosOutputString);
+        }
+
+        public override string ExportSettings()
+        {
+            var stringBuilder = new StringBuilder();
+            var position = (BIPLedPositionEnum)SaitekLEDPosition.Position;
+            stringBuilder.Append("PanelBIP{" + position + "|" + LEDColor + "|" + DCSBiosOutputLED + "}");
+            return stringBuilder.ToString();
+        }
+    }
+}
---
+++ src/NonVisuals/Panels/Saitek/DcsOutputAndColorBindingPZ55.cs
@@ -0,0 +1,65 @@
+﻿namespace NonVisuals.Panels.Saitek {
+    using System;
+    using System.Text;
+    using ClassLibraryCommon;
+    using DCS_BIOS.Serialized;
+    using Panels;
+   
+    [SerializeCriticalCustom]
+    public class DcsOutputAndColorBindingPZ55 : DcsOutputAndColorBinding
+    {
+        public override void ImportSettings(string settings)
+        {
+            if (string.IsNullOrEmpty(settings))
+            {
+                return;
+            }
+
+            if (settings.Contains("SwitchPanelLedUp"))
+            {
+                SaitekLEDPosition = new SaitekPanelLEDPosition((int)SwitchPanelPZ55LEDPosition.UP);
+            }
+            else if (settings.Contains("SwitchPanelLedLeft"))
+            {
+                SaitekLEDPosition = new SaitekPanelLEDPosition((int)SwitchPanelPZ55LEDPosition.LEFT);
+            }
+            else if (settings.Contains("SwitchPanelLedRight"))
+            {
+                SaitekLEDPosition = new SaitekPanelLEDPosition((int)SwitchPanelPZ55LEDPosition.RIGHT);
+            }
+
+            // SwitchPanelLedUp{DARK|DCSBiosOutput{INTEGER_TYPE|Equals|0x0000|0x0000|0|0}}\o/\\?\hid#vid_06a3&pid_0d67#9&231fd360&0&0000#{4d1e55b2-f16f-11cf-88cb-001111000030}
+            settings = settings.Split(Separator, StringSplitOptions.RemoveEmptyEntries)[0];
+
+            // SwitchPanelLedUp{DARK|DCSBiosOutput{INTEGER_TYPE|Equals|0x0000|0x0000|0|0}}
+            settings = settings.Substring(settings.IndexOf("{", StringComparison.InvariantCulture) + 1);
+            settings = settings.Substring(0, settings.Length - 1);
+            var color = settings.Substring(0, settings.IndexOf('|'));
+            settings = settings.Substring(settings.IndexOf('|') + 1);
+            DCSBiosOutputLED = new DCSBIOSOutput();
+            DCSBiosOutputLED.ImportString(settings);
+            LEDColor = (PanelLEDColor)Enum.Parse(typeof(PanelLEDColor), color);
+        }
+
+        public override string ExportSettings()
+        {
+            var stringBuilder = new StringBuilder();
+            if ((SwitchPanelPZ55LEDPosition)SaitekLEDPosition.GetPosition() == SwitchPanelPZ55LEDPosition.UP)
+            {
+                stringBuilder.Append("SwitchPanelLedUp{" + LEDColor + "|" + DCSBiosOutputLED + "}");
+            }
+
+            if ((SwitchPanelPZ55LEDPosition)SaitekLEDPosition.GetPosition() == SwitchPanelPZ55LEDPosition.LEFT)
+            {
+                stringBuilder.Append("SwitchPanelLedLeft{" + LEDColor + "|" + DCSBiosOutputLED + "}");
+            }
+
+            if ((SwitchPanelPZ55LEDPosition)SaitekLEDPosition.GetPosition() == SwitchPanelPZ55LEDPosition.RIGHT)
+            {
+                stringBuilder.Append("SwitchPanelLedRight{" + LEDColor + "|" + DCSBiosOutputLED + "}");
+            }
+
+            return stringBuilder.ToString();
+        }
+    }
+}
