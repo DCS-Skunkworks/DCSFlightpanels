a0738725fb99c3af88aa0fdc8ca2d16759fd87aa
Jerker Dahlblom <jdahlblom@users.noreply.github.com>
2024-03-02 12:12:25 +0200

    update, remove nugets (#496)

--- src/StreamDeckSharp.Tests/StreamDeckIoHardwareTwins.cs
+++ src/StreamDeckSharp.Tests/StreamDeckIoHardwareTwins.cs
@@ -9,7 +9,6 @@ using StreamDeckSharp.Tests.VerifyStuff;
 
 namespace StreamDeckSharp.Tests
 {
-    [UsesVerify]
 #pragma warning disable CS1591 // Missing XML comment for publicly visible type or member
     public class StreamDeckIoHardwareTwins
 #pragma warning restore CS1591 // Missing XML comment for publicly visible type or member
